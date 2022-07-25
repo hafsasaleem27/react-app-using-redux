d792bfd36453e3df0ce62dc3a87c73142af9635e
Hafsa Saleem <hafsa.saleem27@gmail.com>
2022-07-26 03:16:17 +0500

    imported createstore function from redux

--- src/store/index.js
+++ src/store/index.js
@@ -1,4 +1,4 @@
-import redux from "redux";
+import { createStore } from "redux";
 
 const counterReducer = (state = { counter: 0 }, action) => {
   if (action.type === "INCREMENT") {
@@ -14,6 +14,6 @@ const counterReducer = (state = { counter: 0 }, action) => {
   return state;
 };
 
-const store = redux.createStore(counterReducer);
+const store = createStore(counterReducer);
 
 export default store;
