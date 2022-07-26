abc187907a99a3f3892692ae34148a9eb3af9115
Hafsa Saleem <hafsa.saleem27@gmail.com>
2022-07-26 16:41:45 +0500

    added a new piece of state in the redux store

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -4,6 +4,7 @@ import { useSelector, useDispatch, connect } from "react-redux";
 
 const Counter = () => {
   const counter = useSelector(state => state.counter);
+  const show = useSelector(state => state.showCounter);
   const dispatch = useDispatch();
 
   const incrementHandler = () => {
@@ -18,12 +19,14 @@ const Counter = () => {
     dispatch({ type: "INCREASE", value: 5 });
   };
 
-  const toggleCounterHandler = () => {};
+  const toggleCounterHandler = () => {
+    dispatch({ type: "TOGGLE" });
+  };
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>{counter}</div>
+      {show && <div className={classes.value}>{counter}</div>}
       <div>
         <button onClick={incrementHandler}>
           Increment
